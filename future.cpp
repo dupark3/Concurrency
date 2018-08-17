cd33bdf7929eb456dc4a8d5314a24cd644e4c522
DuParkLinux <dupark3@gmail.com>
2018-08-17 11:07:12 -0400

    Throwing exception in thread and using set_exception within that thread's try/catch block to pass the exception handling back to main. Main executes a try block with ftr.get() and catches the error if ftr.get() throws an error.

--- future.cpp
+++ future.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 #include <utility>
 #include <thread>
@@ -7,20 +8,26 @@
 using namespace std;
 
 void thread_function(promise<string> && prms){
-    string str = "hello from future\n";
-    cout << "Str ptr : " << (void *)str.data() << endl;
-    prms.set_value(move(str));
+    try {
+        string str = "hello from future\n";
+        throw(logic_error("error"));
+        prms.set_value(move(str));
+    } catch(...){
+        prms.set_exception(current_exception());
+    }
 }
 
 int main() {
     promise<string> prms;
     future<string> ftr = prms.get_future();
     thread th = thread(&thread_function, move(prms));
-    
-    cout << "hello from main\n";
-    string str = ftr.get();
-    cout << str;
-    cout << "Str ptr : " << (void *)str.data() << endl;
+    try{
+        cout << "hello from main\n";
+        string str = ftr.get();
+        cout << str;
+    } catch (const logic_error& e){
+        cout << e.what() << endl;
+    }
 
     th.join();
         
