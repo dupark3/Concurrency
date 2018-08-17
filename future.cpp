7b2f693746a2983883990d0920c9da8b293f18e3
DuParkLinux <dupark3@gmail.com>
2018-08-17 11:09:59 -0400

    Cleaned up version of exceptions being passed between threads

--- future.cpp
+++ future.cpp
@@ -10,7 +10,7 @@ using namespace std;
 void thread_function(promise<string> && prms){
     try {
         string str = "hello from future\n";
-        throw(logic_error("error"));
+        throw(logic_error("error thrown from the future"));
         prms.set_value(move(str));
     } catch(...){
         prms.set_exception(current_exception());
@@ -20,10 +20,11 @@ void thread_function(promise<string> && prms){
 int main() {
     promise<string> prms;
     future<string> ftr = prms.get_future();
-    thread th = thread(&thread_function, move(prms));
+    thread th(&thread_function, move(prms));
+    
     try{
-        cout << "hello from main\n";
         string str = ftr.get();
+        cout << "hello from main\n";
         cout << str;
     } catch (const logic_error& e){
         cout << e.what() << endl;
